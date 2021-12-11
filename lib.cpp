9005568d7f991d1e8b0d62e8e6b6a2ef5c4fd3df
AlessandroMeschi <91841733+AlessandroMeschi@users.noreply.github.com>
2021-12-11 19:53:16 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,12 @@
 #include "lib.h"
+#include <string.h>
 
-int funzione (char nomi[10][20], char nome[20]){
-  for (int i=0; i<10; i++){
-    bool t=true;
-    for (int j=0; j<20; j++){
-      if (nomi[i][j]!=nome[j])
-        t=false;
+bool res (char list[10][20], char name[20], int&i){
+  for (i=0; i<10; i++){
+    if((strcmp(list[i], name))==0){
+      return true;
     }
-    if(t){
-      return i;
-    }
-    return 0;
     
   }
+  return false;
+}
