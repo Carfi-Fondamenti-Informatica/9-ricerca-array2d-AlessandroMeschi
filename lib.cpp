8d80bee0c44fe493ee77174d8309f3ea95f30c64
AlessandroMeschi <91841733+AlessandroMeschi@users.noreply.github.com>
2021-12-11 19:31:55 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "lib.h"
 
+int funzione (char nomi[10][20], char nome[20]){
+  for (int i=0; i<10; i++){
+    bool t=true;
+    for (int j=0; j<20; j++){
+      if (nomi[i][j]!=nome[j])
+        t=false;
+    }
+    if(t){
+      return i;
+    }
+    return 0;
+    
+  }
