4fe8f8ffcc759e14d28aa5054d61fe8f92b8c267
AlessandroMeschi <91841733+AlessandroMeschi@users.noreply.github.com>
2021-12-11 19:43:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char nomi[10][20], nome[20];
+  cout << " l'utente scelga dieci nomi: ";
+  
+  for(size_t i=0; i<10; i++){
+    cin>> nomi[i];
+    cout << "ancora uno:";
+    for (int j=0; j<20; j++) nome [j];
+    cin >>nome;
+    
+    int posizione = funzione(nomi, nome);
+    if (posizione>=0){
+      cout << posizione;
+    }else{
+      cout << "non presente";
+    }
   return 0;
 }
